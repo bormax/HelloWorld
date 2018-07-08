62ab97624a1e030310a8f1c7dd5a94588bafa9f6
bormax <75max75@mail.ru>
2018-07-08 12:35:18 +0400

    Commit 2

---
+++ IdeaProjects/HelloWorld/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+// Commit 2
+        System.out.println("Hello World!");
+//        for (String s:args) {
+//            System.out.println(s);
+//        }
+    }
+}
