0ff0b1f7a8e3f9769f6cc209f64ce8c59b4150a7
bormax <75max75@mail.ru>
2018-07-08 12:37:28 +0400

    Commit 3

--- IdeaProjects/HelloWorld/src/com/company/Main.java
+++ IdeaProjects/HelloWorld/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-// Commit 2
+// Commit 3
         System.out.println("Hello World!");
 //        for (String s:args) {
 //            System.out.println(s);
